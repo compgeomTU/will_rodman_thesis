d2448a67fdc7599cc80da069a1140dc64bed12b9
Will Rodman <willc.rodman@gmail.com>
2024-04-01 00:12:07 -0400

    update error .

--- TraversalDistance/KNeighborsClassifier.py
+++ TraversalDistance/KNeighborsClassifier.py
@@ -1,5 +1,6 @@
 from .BinarySearch import BinarySearch
 from collections import Counter
+from multiprocessing import Pool
 
 class KNeighborsClassifier():
     mean: object
@@ -8,7 +9,6 @@ class KNeighborsClassifier():
 
     X: list
     y: list
-    distances: list
     
     left: float
     right: float
@@ -17,7 +17,6 @@ class KNeighborsClassifier():
     __log: list
 
     def __init__(self, n_neighbors, mean='arithmetic', left=0, right=1000, precision=1):
-        self.distances = list()
         self.n_neighbors = n_neighbors
                 
         self.left = left
@@ -58,34 +57,47 @@ class KNeighborsClassifier():
         return self.mean(epsilon_a, epsilon_b)     
     
     def __compute(self, x_pred):
-        self.distances.clear()
-        #print("\n*** Computing Prediction ***")
+        distances = list()
                 
         # Computing distances. 
         for x in self.X:
             distance = self.__metric(x_pred, x)
-            self.distances.append(distance)
+            distances.append(distance)
             
         # Sorting distances. 
-        sorted_indices = sorted(enumerate(self.distances), key=lambda x: x[1])
+        sorted_indices = sorted(enumerate(distances), key=lambda x: x[1])
         
         # Indices of the n smallest distances. 
         n_indices = [index for index, _ in sorted_indices[:self.n_neighbors]]
 
         # Create a sublist of classifications using indices.
         n_classifications = [self.y[i] for i in n_indices]
-        #print("Nearest Classifications:", n_classifications)
         
         # Most common classifications in list => prediction. 
         y_hat, _ = Counter(n_classifications).most_common(1)[0]
-        #print("Predicted Classification:", y_hat)
         
         self.__log.append((y_hat, n_classifications))
+        
         return y_hat
     
     def fit(self, X, y):
         self.X = X
         self.y = y
-        
-    def predict(self, X):
+       
+    def bad_predict(self, X):        
         return list(map(self.__compute, X)), self.__log
+    
+    # does not work. 
+    def multi_predict(self, X, n_processes=1):        
+        k, m = divmod(len(X), n_processes)
+        batches = list(X[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n_processes))
+
+        def task(batch):
+            return [self.__compute(x) for x in batch]
+        
+        with Pool(processes=n_processes) as pool:
+            results = pool.map(task, batches)
+            
+        predictions = [item for sublist in results for item in sublist]
+        
+        return predictions, self.__log
--- TraversalDistance/LineIntersection.py
+++ TraversalDistance/LineIntersection.py
@@ -188,8 +188,15 @@ def find_ellipse_max_min_points(line1, line2, epsilon, debug=False):
 
     cos_alpha = (v1[0] * v2[0] + v1[1] * v2[1]) / (math.sqrt(v1[0]
                                                              ** 2 + v1[1] ** 2) * math.sqrt(v2[0] ** 2 + v2[1] ** 2))
-    sin_alpha = math.sqrt(1-cos_alpha ** 2)
-    d = abs(epsilon / sin_alpha)
+    
+    ### DEBUG: WILL RODMAN 3/31/24 ###
+    if abs(cos_alpha) >= 1:
+        d = epsilon
+    else:
+        sin_alpha = math.sqrt(1-cos_alpha ** 2)
+        d = abs(epsilon / sin_alpha)
+    ### DEBUG: WILL RODMAN 3/31/24 ###
+    
 
     m1 = slope(line1[0][0], line1[0][1], line1[1][0], line1[1][1])
     m2 = slope(line2[0][0], line2[0][1], line2[1][0], line2[1][1])
